26070b47dcffb33e12d70078c89cb4ed6d95062a
dustinpfister <dustin.pfister@gmail.com>
2021-09-16 15:49:46 -0400

    createSource method is working good

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const path = require('path'),
-
 buildTool = require( path.join(__dirname, 'lib/build-tool.js') );
 
+
+let opt = {
+    dir_root: path.join(__dirname, 'demo'),
+    dir_target: 'dist',
+    sourceFiles: [
+     "./src/file1.js", "./src/file2.js", "./src/file3.js"
+    ]
+};
+
+// create source
+buildTool.createSource(opt)
+// what to do with source object
+.then((source)=>{
+   opt.sourceCode = source.code;
+   console.log(buildTool.createDistObj(opt) )
+});
+
+
+/*
 let distObj = buildTool.createDistObj({
     sourceCode : 'var n = 15;   \n\n\n var z = 5;'
 });
 
 console.log( distObj );
+*/
+
