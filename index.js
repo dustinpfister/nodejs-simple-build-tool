f9bee228de8ae0a098b6a5e976471f5a3304e615
dustinpfister <dustin.pfister@gmail.com>
2021-09-17 15:36:11 -0400

    index.js clean up

--- index.js
+++ index.js
@@ -4,49 +4,27 @@ buildTool = require( path.join(__dirname, 'lib/build-tool.js') );
 let uri_build_conf = process.argv[2] ||  path.join(process.cwd(), 'build-conf.json');
 
 
-let opt = {
-    //fileName: "test_script",
-    //dir_root: process.cwd(),
-    //dir_target: 'dist'
-    //sourceFiles: [
-    // "./src/file1.js", "./src/file2.js", "./src/file3.js"
-    //],
-    //version: '0.0.0'
-};
+// the options object
+let opt = {};
 
 // start by reading the json file
 buildTool.readConf(uri_build_conf)
+// append build-conf.json values to opt and create source
 .then((conf)=>{
-   console.log(conf);
    opt = Object.assign(opt, conf);
-   console.log(opt);
    return buildTool.createSource(opt);
 })
+// append opt.sourceCode and create dist options by calling createDist
+// then write dist
 .then((source)=>{
    opt.sourceCode = source.code;
    let dist = buildTool.createDistObj(opt);
    return buildTool.writeDist(dist);
 })
-.then((obj) => {
-    //console.log(obj);
+.then((dist) => {
+    console.log('dist folder created : ');
+    console.log('path: ' + dist.dir_target);
 })
 .catch((e) => {
-    //console.log(e);
+    console.log(e);
 });
-
-/*
-// create source
-buildTool.createSource(opt)
-// what to do with source object
-.then((source)=>{
-   opt.sourceCode = source.code;
-   let dist = buildTool.createDistObj(opt);
-   return buildTool.writeDist(dist);
-})
-.then((obj) => {
-    //console.log(obj);
-})
-.catch((e) => {
-    //console.log(e);
-});
-*/
