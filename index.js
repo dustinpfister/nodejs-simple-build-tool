2b79488dc7c61d6b87643e3b9a8a7a5f3d451a1e
dustinpfister <dustin.pfister@gmail.com>
2021-09-17 16:29:26 -0400

    clean up

--- index.js
+++ index.js
@@ -11,29 +11,3 @@ buildTool.build(uri_build_conf)
 .catch((e) => {
     console.log(e);
 });
-
-/*
-// the options object
-let opt = {};
-// start by reading the json file
-buildTool.readConf(uri_build_conf)
-// append build-conf.json values to opt and create source
-.then((conf)=>{
-   opt = Object.assign(opt, conf);
-   return buildTool.createSource(opt);
-})
-// append opt.sourceCode and create dist options by calling createDist
-// then write dist
-.then((source)=>{
-   opt.sourceCode = source.code;
-   let dist = buildTool.createDistObj(opt);
-   return buildTool.writeDist(dist);
-})
-.then((dist) => {
-    console.log('dist folder created : ');
-    console.log('path: ' + dist.dir_target);
-})
-.catch((e) => {
-    console.log(e);
-});
-*/
